c7910d0ce56c79141ff5cbf18f41d41fbc79945a
lainecedes <elaine.parieh@gmail.com>
2024-02-09 18:46:20 +0100

    changes to script.js, npm packages and minor changes in html/css

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express'); // express
+const dotenv = require('dotenv'); // dotenv
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)); // node-fetch, can't use require
+const port = process.env.PORT || 3000; // port
+const app = express();
+
+// dotenv configuration
+// require("dotenv").config({ path: "/.env" });
+dotenv.config({ path: "./.env" });
+
+const apiKey = process.env.API_KEY; // retrieve API key from .env file
+console.log("API Key:", apiKey);
+
+
+
+// middleware voor nookipedia api key
+// const checkApiKey = (req, res, next) => {
+// const providedKey = req.query.apiKey;
+
+//   if (!providedKey || providedKey !== apiKey) {
+//     return res.status(403).json({ error: 'Invalid API key'});
+    
+//   }
+//   next();
+// };
+
+// de static files met express.static
+app.use(express.static('/docs'));
+
+
+// index.html + Your API key-protected endpoint
+app.get('/', (req, res) => {
+        let url = `https://api.nookipedia.com/villagers?name=ace&api_key=${apiKey}`;
+        let options = {
+        method: "GET",
+        // headers: { "x-api-key": apiKey },
+    };
+        fetch(url, options)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(`error ${err}`);
+    });
+    res.render("/docs/index.html")
+});
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
